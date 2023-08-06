0b98ea014637591e943d0969fc4992423d60b1c1
Genesis Lara <genesislara@pursuit.org>
2023-08-06 10:49:50 -0400

    works on checkout form

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import React from "react";
-import Cart from "./Components/Cart.jsx";
+// import Cart from "./Components/Cart.jsx";
 import Checkout from "./Components/Checkout.jsx";
-import BirdCards from "./Components/BirdCards.jsx";
-
+// import BirdCards from "./Components/BirdCards.jsx";
+import { useState } from "react";
 
 function App () {
+  // const [bird, setBird] = useState(birdData);
+  // const [cart, setCart] = useState()
+
   return (
     <div>
-      <Cart />
+      {/* <Cart /> */}
       <Checkout />
-      <BirdCards />
+      {/* <BirdCards /> */}
     </div>
   );
 };
--- src/Components/Checkout.jsx
+++ src/Components/Checkout.jsx
@@ -1,8 +1,65 @@
 import React from "react";
 
+function Checkout() {
+  const [user, setUser] = useState({
+    firstName: " ",
+    lastName: " ",
+    zip: " ",
+    email: " ",
+    zipcode: " ",
+  })
+  function handleSubmit(e) {
+    event.preventDefault();
+    resetForm()
+  }
+
+  function resetForm() {
+    setUser({
+      firstName: " ",
+      lastName: " ",
+      zip: " ",
+      email: " ",
+      zipcode: " ",
+    })
+  }
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <h3>Checkout</h3>
+        <label htmlFor="name">First Name</label>
+        <input
+          type="text"
+          id="name"
+        />
+
+        <label htmlFor="name">Last Name</label>
+        <input
+          type="text"
+          id="name"
+        />
+
+        <label htmlFor="name">Email</label>
+        <input
+          type="text"
+          id="email"
+        />
+
+        <label htmlFor="name">Zip Code</label>
+        <input
+          type="number"
+          id="zipcode"
+        />
+        <br />
+        <input type="submit" />
+      </form>
+    </>
+  )
+}
+
+
 // form to checkout, resets upon checkout
-// when form is completed, 'Submit' and alert is successful
-// `You have adopted birds. Thank you!`
+// when form is completed, 'Submit': `You have adopted birds. Thank you!`
 // when alert box closed, cart should FULLY reset
 
 
